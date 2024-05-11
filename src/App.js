def401f3e465a1778ae1893bdd81298824ed1b82
Diyouf <diyoufkv7@gmail.com>
2024-05-11 12:37:13 +0530

    learning(hooks):useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import "./App.css";
 import Navbar from "./component/Navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
   const [Count, setCount] = useState(0);
   const [name, setName] = useState("Diyouf");
+  const [ChildData, setChildData] = useState("2");
+  const [sum, setsum] = useState(0);
+
+  useEffect(() => {
+    const sum = 10 + Number(ChildData);
+    setsum(sum);
+    return () => {};
+  }, [ChildData]);
 
   function increamentCount() {
     setCount(Count + 1);
   }
 
   function decreamentCount() {
-    if (Count <= 0) return alert("Invalid");
+    if (Count <= 0) return alert("invalid");
 
     setCount(Count - 1);
   }
@@ -19,11 +27,18 @@ export default function App() {
   function changeName() {
     setName("john");
   }
+
+  const handleMessageFromChild = (message) => {
+    setChildData(message);
+  };
+
   return (
     <div className="App">
-      <Navbar name={name} />
+      <Navbar name={name} sendMessageToParent={handleMessageFromChild} />
       <p>{Count}</p>
       <p>{name}</p>
+      <h1>{sum}</h1>
+      <p>From Child : {ChildData}</p>
       <button onClick={changeName}>change Name</button>
       <button onClick={increamentCount}>Increment</button>
       <button onClick={decreamentCount}>Increment</button>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 export default function Navbar(props) {
+  const [message, setmessage] = useState("");
+  
+
+  const handleInputData = (event) => {
+    setmessage(event.target.value);
+  };
+
+  const sendMessage = () => {
+    props.sendMessageToParent(message);
+  };
+
   return (
     <div>
-      <p>Navbar</p>
+      <p>NavBar</p>
       <p>{props.name}</p>
+      <input type="text" value={message} onChange={handleInputData} />
+      <button type="submit" onClick={sendMessage}>
+        Save
+      </button>
     </div>
   );
 }
